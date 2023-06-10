1c3c222ef1b5f86a5540c77d86bc089b76ec8e2a
gitmazter <philip.andelic@gmail.com>
2023-06-10 13:38:09 +0700

    Nice

--- components/PingButton.tsx
+++ components/PingButton.tsx
@@ -1,16 +1,63 @@
-import { FC, useState } from 'react'
+import { FC, useEffect, useState } from 'react'
 import styles from '../styles/PingButton.module.css'
+import * as web3 from '@solana/web3.js'
+import { useConnection, useWallet } from '@solana/wallet-adapter-react' 
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui'
+
 
 export const PingButton: FC = () => {
+	const { connection } = useConnection();
+	const { publicKey, sendTransaction } = useWallet();
+	const [ solscanLink, setSolscanLink ] = useState("")
 
-    const onClick = () => {
-        console.log('Ping!')
+	useEffect(() => {
+
+	}, [solscanLink])
+
+	const clearData = () => {
+	}
+
+    const onClick = async() => {
+        if (!connection || !publicKey) {return}
+		
+		const programId = new web3.PublicKey(process.env.NEXT_PROGRAM_ID);
+		const programDataAcc = new web3.PublicKey(process.env.NEXT_DATA_ACCOUNT);
+
+		console.log(programId);
+		console.log(programDataAcc);
+		
+		const transaction = new web3.Transaction()
+		const transactionInstructions = new web3.TransactionInstruction({
+			keys: [
+				{
+					pubkey: programDataAcc,
+					isSigner: false,
+					isWritable: true
+				}
+			],
+			programId
+		})
+
+		transaction.add(transactionInstructions)
+		sendTransaction(transaction, connection).then(sig => {
+			setSolscanLink(`https://solscan.io/tx/${sig}?cluster=devnet`)
+			//window.open(`https://solscan.io/tx/${sig}?cluster=devnet`)
+			console.log(sig);
+		})
+		
     }
     
 	return (
-		<div className={styles.buttonContainer} onClick={onClick}>
-			<button className={styles.button}>Ping!</button>
-		</div>
+		publicKey ? 
+		<>
+			<div className={styles.buttonContainer} onClick={onClick}>
+				<button className={styles.button}>Ping!</button>
+			</div>
+			<a href={solscanLink} onClick={clearData} target="_blank" className='pingReceipt'>{solscanLink ? "Solscan Receipt" : ""}</a>
+		</>
+
+		:
+		<WalletMultiButton/>
 	)
 }
 
--- next.config.js
+++ next.config.js
@@ -3,4 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  nextConfig,
+  env: {
+    NEXT_PROGRAM_ID:'ChT1B39WKLS8qUrkLvFDXMhEJ4F1XZzwUNHUt4AU9aVa',
+    NEXT_DATA_ACCOUNT:'Ah9K7dQ8EHaZqcAsgBW8w37yN2eAy3koFmUn4x3CJtod'
+  }
+}
